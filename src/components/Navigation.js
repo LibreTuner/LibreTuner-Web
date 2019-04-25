5e17248dd4179e97942c3c45ab9491d4b61696cd
Altenius <jacobjm18@gmail.com>
2019-04-25 11:42:44 -0400

    Added navigation container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,29 +9,33 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 import './Navigation.css'
 
 function Navigation() {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">
-            <Logo/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-                <NavDropdown title="Tune" id="basic-nav-dropdown">
-                    <NavDropdown.Item as={Link} to="/tune/edit">Edit Tune</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search Tune</Button>
-            </Form>
-        </Navbar.Collapse>
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <Logo/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+                        <NavDropdown title="Tune" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/tune/edit">Edit Tune</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search Tune</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
+        
     )
 }
 
